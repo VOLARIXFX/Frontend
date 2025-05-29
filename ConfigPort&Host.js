cc154cb135915fd8d7200d31afe71474847f9099
VOLARIXFX <volarixfx@gmail.com>
2025-05-28 21:23:34 -0500

    Update ConfigPort&Host.js

--- ConfigPort&Host.js
+++ ConfigPort&Host.js
@@ -1,4 +1,4 @@
-const API_HOST = import.meta.env.VITE_API_HOST;
+const API_HOST = 'https://backend-06si.onrender.com';
 // const API_PORT = import.meta.env.VITE_API_PORT || '3000';
 
 export const API_URL = `${API_HOST}`;
