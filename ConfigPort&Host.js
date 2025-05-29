86a4f141bb99c53cd272a8fc6566621f08725401
VOLARIXFX <volarixfx@gmail.com>
2025-05-28 20:59:12 -0500

    Cambios variables y puerto

--- ConfigPort&Host.js
+++ ConfigPort&Host.js
@@ -1,4 +1,12 @@
 const API_HOST = import.meta.env.VITE_API_HOST || 'localhost';
 const API_PORT = import.meta.env.VITE_API_PORT || '3000';
 
-export const API_URL = `http://${API_HOST}:${API_PORT}`;
+let API_URL = '';
+
+if (API_HOST.startsWith('http')) {
+  API_URL = API_HOST; // ya es un dominio completo con protocolo
+} else {
+  API_URL = `http://${API_HOST}:${API_PORT}`;
+}
+
+export { API_URL };
