02b9986fd592d8f3873318d0b2c2ea947d3b885d
VOLARIXFX <volarixfx@gmail.com>
2025-05-28 21:43:44 -0500

    Huesa

--- ConfigPort&Host.js
+++ ConfigPort&Host.js
@@ -1,4 +1,3 @@
-const API_HOST = 'https://backend-06si.onrender.com';
-// const API_PORT = import.meta.env.VITE_API_PORT || '3000';
+const API_HOST = import.meta.env.VITE_API_HOST || 'http://localhost:3000';
 
-export const API_URL = `${API_HOST}`;
+export const API_URL = API_HOST;
