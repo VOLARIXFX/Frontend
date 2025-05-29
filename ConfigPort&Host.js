4543c111777d76738f151c813fc25de8e00ee10f
VOLARIXFX <volarixfx@gmail.com>
2025-05-28 21:11:04 -0500

    Nuevos cambios url consumo backend

--- ConfigPort&Host.js
+++ ConfigPort&Host.js
@@ -1,12 +1,4 @@
-const API_HOST = import.meta.env.VITE_API_HOST || 'localhost';
-const API_PORT = import.meta.env.VITE_API_PORT || '3000';
+const API_HOST = import.meta.env.VITE_API_HOST;
+// const API_PORT = import.meta.env.VITE_API_PORT || '3000';
 
-let API_URL = '';
-
-if (API_HOST.startsWith('http')) {
-  API_URL = API_HOST; // ya es un dominio completo con protocolo
-} else {
-  API_URL = `http://${API_HOST}:${API_PORT}`;
-}
-
-export { API_URL };
+export const API_URL = `${API_HOST}`;
